c171cdc2449464c38260c618970a4313a4f6446e
Nik <nik_0704@bk.ru>
2020-04-21 23:23:29 +0300

    create config file

--- conftest.py
+++ conftest.py
@@ -1,19 +1,24 @@
 import pytest
 from fixture.application import Application
+import json
+import os.path
 
 fixture = None
+config = None
 
 
 @pytest.fixture()
 def app(request):
     global fixture
+    global config
     browser = request.config.getoption("--browser")
-    if fixture is None:
+    if config is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--config"))
+        with open(config_file) as file:
+            config = json.load(file)
+    if fixture is None or not fixture.is_valid():
         fixture = Application(browser=browser)
-    else:
-        if not fixture.is_valid():
-            fixture = Application(browser=browser)
-    fixture.session.ensure_login(username="admin", password="secret")
+    fixture.session.ensure_login(username=config["username"], password=config["password"])
     return fixture
 
 
@@ -29,3 +34,4 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="chrome")
+    parser.addoption("--config", action="store", default="config.json")
